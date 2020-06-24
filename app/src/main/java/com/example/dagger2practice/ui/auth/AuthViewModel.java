13ed004dc7130023771b9ff56595144c683124b3
Mason Ashment <masonashment@gmail.com>
2020-06-24 18:10:55 -0400

    Make Retrofit call with RxJava

--- app/src/main/java/com/example/dagger2practice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/example/dagger2practice/ui/auth/AuthViewModel.java
@@ -4,11 +4,16 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.dagger2practice.models.User;
 import com.example.dagger2practice.network.auth.AuthApi;
 
 import java.util.List;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -20,6 +25,31 @@ public class AuthViewModel extends ViewModel {
     public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: AuthApi is " + (authApi != null));
+
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(User value) {
+                        Log.d(TAG, "onNext: " + value.getEmail());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "onError: ",e );
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
 }
